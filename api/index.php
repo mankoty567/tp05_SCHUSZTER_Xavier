212c32c25188b33b9bd4a6fb67885bcd1bfec168
Xavier Schuszter <schuszter@etu.unistra.fr>
2022-01-27 16:38:41 +0100

    V1'

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
-echo "hello world"
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+
+require __DIR__ . '/../vendor/autoload.php';
+ 
+$app = AppFactory::create();
+
+
+$app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
+    $array = [];
+    $array ["nom"] = $args ['name'];
+    $response->getBody()->write(json_encode ($array));
+    return $response;
+});
+
+
+
+$app->run ();
